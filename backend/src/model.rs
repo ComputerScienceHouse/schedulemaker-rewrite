0e98474806b8079b0aed3c5ff9971cda29622c8e
Sam Cordry <samcordry@gmail.com>
2024-02-22 14:21:26 -0500

    Finalize model for all tables

--- backend/src/model.rs
+++ backend/src/model.rs
@@ -7,13 +7,13 @@
 //! backend queries for its information.
 
 use serde::{Deserialize, Serialize};
-use sqlx::{mysql, query_builder::Separated, types::chrono, FromRow, Postgres};
-use std::fmt::Display;
+use sqlx::{query_builder::Separated, Postgres, FromRow};
+use std::fmt;
 use utoipa::ToSchema;
 
 pub trait ToRow {
     fn keys() -> &'static [&'static str];
-    fn bind<'qb, 'args: 'qb, Sep: Display>(
+    fn bind<'qb, 'args: 'qb, Sep: fmt::Display>(
         &self,
         query_builder: &mut Separated<'qb, 'args, Postgres, Sep>,
     );
@@ -40,7 +40,7 @@ macro_rules! generate_to_row {
         #[doc = $doc:expr]
         pub struct $name:ident {
             $(
-                $(#[doc = field_doc:expr])?
+                $(#[doc = $field_doc:expr])?
                 $field_name:ident: $field_type:ty,
             )*
         }
@@ -59,7 +59,7 @@ macro_rules! generate_to_row {
                 &[$(stringify!($field_name)),*]
             }
 
-            fn bind<'qb, 'args: 'qb, Sep: Display>(
+            fn bind<'qb, 'args: 'qb, Sep: fmt::Display>(
                 &self,
                 query_builder: &mut Separated<'qb, 'args, Postgres, Sep>
             ) {
@@ -67,6 +67,159 @@ macro_rules! generate_to_row {
                 $(.push_bind(self.$field_name.to_encode()))*;
             }
         }
+    }
+}
+
+generate_to_row! {
+    /// Represents the record of academic terms
+    pub struct AcademicTermRecord {
+        term: i16,
+        start: chrono::NaiveDate,
+        end: chrono::NaiveDate,
+    }
+}
+
+generate_to_row! {
+    /// Represents the record of buildings
+    pub struct BuildingRecord {
+        number: String,
+        code: String,
+        name: String,
+        off_campus: bool,
+    }
+}
+
+generate_to_row! {
+    /// Represents the courses that are recorded
+    pub struct CourseRecord {
+        id: i32,
+        term: i16,
+        department: i32,
+        course: String,
+        credits: i8,
+        title: String,
+        description: String,
+    }
+}
+
+generate_to_row! {
+    /// Represents the academic departments a class can be offered by
+    pub struct DepartmentRecord {
+        id: i32,
+        school: i32,
+        code: String,
+        title: String,
+    }
+}
+
+generate_to_row! {
+    /// Represents a part of a schedule that is a course
+    pub struct ScheduleCourseRecord {
+        id: i32,
+        schedule: i32,
+        section: i32,
+    }
+}
+
+generate_to_row! {
+    /// Represents a part of a schedule that is not a course
+    pub struct ScheduleNonCourseRecord {
+        id: i32,
+        schedule: i32,
+        title: String,
+        day: i8,
+        start: i16,
+        end: i16,
+    }
+}
+
+generate_to_row! {
+    /// Represents created schedules
+    pub struct ScheduleRecord {
+        id: i32,
+        last_accessed: chrono::DateTime<chrono::Utc>,
+        start_day: i8,
+        end_day: i8,
+        start_time: i8,
+        end_time: i8,
+        term: i16,
+        image: bool,
+    }
+}
+
+generate_to_row! {
+    /// Represents the schools that a course can be offered by
+    pub struct SchoolRecord {
+        id: i32,
+        code: String,
+        title: String,
+    }
+}
+
+generate_to_row! {
+    /// Represents data about the parsing process
+    pub struct ScrapeRecord {
+        id: i32,
+        time_started: i32,
+        time_ended: i32,
+        terms_added: i8,
+        courses_added: i32,
+        courses_updated: i32,
+        sections_added: i32,
+        failures: i32,
+    }
+}
+
+generate_to_row! {
+    /// Represents record about a section of a class
+    pub struct SectionRecord {
+        id: i32,
+        course: i32,
+        section: String,
+        title: String,
+        section_type: SectionType,
+        status: SectionStatus,
+        instructor: String,
+        maxenroll: i16,
+        curenroll: i16,
+    }
+}
+
+/// Represents the type of section
+#[derive(Debug, Serialize, Deserialize, sqlx::Type, Clone, ToSchema)]
+pub enum SectionType {
+    #[serde(rename = "R")]
+    Regular,
+    #[serde(rename = "N")]
+    None,
+    #[serde(rename = "OL")]
+    Online,
+    #[serde(rename = "H")]
+    Honors,
+    #[serde(rename = "BL")]
+    Blended,
+}
+
+impl Default for SectionType {
+    fn default() -> Self {
+        Self::Regular
+    }
+}
+
+/// Represents the current status of the section
+#[derive(Debug, Serialize, Deserialize, sqlx::Type, Clone, ToSchema)]
+pub enum SectionStatus {
+    #[serde(rename = "O")]
+    Open,
+    #[serde(rename = "C")]
+    Closed,
+    #[serde(rename = "X")]
+    Cancelled,
+}
+
+impl Default for SectionStatus {
+    fn default() -> Self {
+        Self::Open
     }
 }
 
@@ -146,168 +299,65 @@ impl Default for SchedulePrint {
 }
 
 generate_to_row! {
-    /// Represents the record of academic terms
-    pub struct AcademicTermRecord {
-        term: u16,
-        start: chrono::Date,
-        end: chrono::Date,
-    }
-}
-
-generate_to_row! {
-    /// Represents the record of buildings
-    pub struct BuildingRecord {
-        number: String,
-        code: String,
-        name: String,
-        off_campus: bool,
-    }
-}
-
-generate_to_row! {
-    /// Represents the courses that are recorded
-    pub struct CourseRecord {
-        id: u32,
-        term: u16,
-        department: u32,
-        course: String,
-        credits: u8,
-        title: String,
-        description: String,
-    }
-}
-
-generate_to_row! {
-    /// Represents the academic departments a class can be offered by
-    pub struct DepartmentRecord {
-        id: u32,
-        school: u32,
-        code: String,
-        title: String,
-    }
-}
-
-generate_to_row! {
-    /// Represents a part of a schedule that is a course
-    pub struct ScheduleCourseRecord {
-        id: u32,
-        schedule: u32,
-        section: u32,
-    }
-}
-
-generate_to_row! {
-    /// Represents a part of a schedule that is not a course
-    pub struct ScheduleNonCourseRecord {
-        id: u32,
-        schedule: u32,
-        title: String,
-        day: u8,
-        start: u16,
-        end: u16,
-    }
-}
-
-generate_to_row! {
-    /// Represents created schedules
-    pub struct ScheduleRecord {
-        id: u32,
-        last_accessed: chrono::DateTime<chrono::Utc>,
-        start_day: u8,
-        end_day: u8,
-        start_time: u8,
-        end_time: u8,
-        term: u16,
-        image: bool,
-    }
-}
-
-generate_to_row! {
-    /// Represents the schools that a course can be offered by
-    pub struct SchoolRecord {
-        id: u32,
-        code: String,
-        title: String,
-    }
-}
-
-generate_to_row! {
-    /// Represents data about the parsing process
-    pub struct ScrapeRecord {
-        id: u32,
-        time_started: u32,
-        time_ended: u32,
-        terms_added: u8,
-        courses_added: u32,
-        courses_updated: u32,
-        sections_added: u32,
-        failures: u32,
-    }
-}
-
-generate_to_row! {
-    /// Represents record about a section of a class
-    pub struct SectionRecord {
-        id: u32,
-        course: u32,
-        section: String,
-        title: String,
-        type: SectionType,
-        status: SectionStatus,
-        instructor: String,
-        maxenroll: u16,
-        curenroll: u16,
-    }
-}
-
-/// Represents the type of section
-#[derive(Debug, Serialize, Deserialize, sqlx::Type, Clone, ToSchema)]
-pub enum SectionType {
-    #[serde(rename = "R")]
-    Regular,
-    #[serde(rename = "N")]
-    None,
-    #[serde(rename = "OL")]
-    Online,
-    #[serde(rename = "H")]
-    Honors,
-    #[serde(rename = "BL")]
-    Blended,
-}
-
-impl Default for SectionType {
-    fn default() -> Self {
-        Self::Regular
-    }
-}
-
-/// Represents the current status of the section
-#[derive(Debug, Serialize, Deserialize, sqlx::Type, Clone, ToSchema)]
-pub enum SectionStatus {
-    #[serde(rename = "O")]
-    Open,
-    #[serde(rename = "C")]
-    Closed,
-    #[serde(rename = "X")]
-    Cancelled,
-}
-
-impl Default for SectionStatus {
-    fn default() -> Self {
-        Self::Open
-    }
-}
-
-generate_to_row! {
-    /// Represents a meeting for a section of a class
+    /// Describes the meeting schedule of a course.
     pub struct MeetingRecord {
-        id: u32,
-        section: u32,
-        day: u8,
-        start: u16,
-        end: u16,
+        course_id: i32,
+        course_offer_number: i32,
+        academic_term: i32,
+        session_code: String,
+        class_section: String,
+        class_meeting_number: i32,
+        start_date: String,
+        end_date: String,
         building: String,
-        room: String,
+        room_number: String,
+        meeting_time_start:  String,
+        meeting_time_end: String,
+        monday: WeekdayScheduled,
+        tuesday: WeekdayScheduled,
+        wednesday: WeekdayScheduled,
+        thursday: WeekdayScheduled,
+        friday: WeekdayScheduled,
+        saturday: WeekdayScheduled,
+        sunday: WeekdayScheduled,
+    }
+}
+
+/// Represents whether a section meets on a given day of the week.
+#[derive(Debug, Serialize, Deserialize, sqlx::Type, Clone, ToSchema)]
+pub enum WeekdayScheduled {
+    #[serde(rename = "Y")]
+    Scheduled,
+    #[serde(rename = "N")]
+    NotScheduled,
+}
+
+impl Default for WeekdayScheduled {
+    fn default() -> Self {
+        Self::Scheduled
+    }
+}
+
+impl fmt::Display for WeekdayScheduled {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Scheduled => write!(f, "Y"),
+            NotScheduled => write!(f, "N"),
+        }
+    }
+}
+
+generate_to_row! {
+    /// Associates instructors to the courses they teach.
+    pub struct InstructorRecord {
+        course_id: i32,
+        course_offer_number: i32,
+        academic_term: i32,
+        session_code: String,
+        class_section: String,
+        class_meeting_number: i32,
+        last_name: String,
+        first_name: String,
     }
 }
 
